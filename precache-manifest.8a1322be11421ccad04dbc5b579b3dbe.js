32ed0bc19d619bf28a13034d78ae36040f5d071b
Sanil Rama <sanil96.sr@gmail.com>
2020-09-13 19:29:18 +0100

    Updates

---
+++ precache-manifest.8a1322be11421ccad04dbc5b579b3dbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f443c09c6d2de99f41e807876a55d9ef",
+    "url": "/SplitBills/index.html"
+  },
+  {
+    "revision": "9f4b96989f81725f5eb1",
+    "url": "/SplitBills/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b940ec8cee2bd4a122ab",
+    "url": "/SplitBills/static/css/main.6b4bc025.chunk.css"
+  },
+  {
+    "revision": "9f4b96989f81725f5eb1",
+    "url": "/SplitBills/static/js/2.1c254df4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SplitBills/static/js/2.1c254df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b940ec8cee2bd4a122ab",
+    "url": "/SplitBills/static/js/main.ada9e13b.chunk.js"
+  },
+  {
+    "revision": "d23618676d5e51e4665b",
+    "url": "/SplitBills/static/js/runtime-main.9e96306e.js"
+  }
+]);
